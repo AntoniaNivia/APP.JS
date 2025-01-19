dab7704a329f8264f8a2b7ee9f03d327c0bd0586
AntoniaNivia <capic.2023118ISINF0012@aluno.ifpi.edu.br>
2025-01-19 18:05:11 -0300

    go

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-import cors from 'cors'
-import Express from 'express'
-import logger from './middlewares/logger.js'
-import userRouter from './router/users.js'
+import cors from 'cors';
+import Express from 'express';
+import logger from './middlewares/logger.js';
+import userRouter from './router/users.js';
 
+const cors= require('cors')
 const app = Express()
-app.use(cors())
 app.use(Express.json())
 
-app.use(logger)
-
+app.use(cors({
+  origin: 'https://ifpi-picos.github.io',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}))
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send("App online!")
 })
 
-
+app.use(logger)
 app.use(userRouter)
 
-
 const PORT = process.env.PORT || 3000
+
 app.listen(PORT, () => {
-    console.log('app online na porta 3000')
+  console.log(`app online na porta ${PORT}`)
 });
